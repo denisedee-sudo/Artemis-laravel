c04ba8a5e590caa061030ddb92471609bc144fd3
Denise De Jesus <denisedeedejesus@pm.me>
2022-10-29 02:05:10 +0800

    tweaked text

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -2,6 +2,6 @@
     <br><br><br>
     <div class="container px-5 py-5 text-center">
         <h1>Thank you</h1>
-        <p>You reservation is ready.</p>
+        <p>You appointment is ready.</p>
     </div>
 </x-guest-layout>
