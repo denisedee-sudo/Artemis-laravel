9750121b9c0111fe5a72868c9810cd56d253a5b5
Denise De Jesus <denisedeedejesus@pm.me>
2022-10-28 15:01:59 +0800

    functional

--- resources/views/menus/index.blade.php
+++ resources/views/menus/index.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <div class="container w-full px-5 py-6 mx-auto">
         <div class="grid lg:grid-cols-4 gap-y-6">
-            @foreach ($menus as $menu)
+            {{-- @foreach ($menus as $menu)
                 <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
                     <img class="w-full h-48" src="{{ Storage::url($menu->image) }}" alt="Image" />
                     <div class="px-6 py-4">
@@ -13,7 +13,7 @@
                         <span class="text-xl text-green-600">${{ $menu->price }}</span>
                     </div>
                 </div>
-            @endforeach
+            @endforeach --}}
         </div>
     </div>
 </x-guest-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,7 +121,7 @@
         </div>
         <div class="container w-full px-5 py-6 mx-auto">
             <div class="grid lg:grid-cols-4 gap-y-6">
-                @foreach ($specials->menus as $menu)
+                {{-- @foreach ($specials->menus as $menu)
                     <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
                         <img class="w-full h-48" src="{{ Storage::url($menu->image) }}" alt="Image" />
                         <div class="px-6 py-4">
@@ -133,7 +133,7 @@
                             <span class="text-xl text-green-600">${{ $menu->price }}</span>
                         </div>
                     </div>
-                @endforeach
+                @endforeach --}}
             </div>
         </div>
     </section>
